230ecf252259c77d6dd4c54f32d6fc8f033c4dc2
Giuseppe Leo <giuseppe.leo@retresco.de>
2021-08-27 00:47:29 +0200

    Add statics

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const chart = [
+    {
+      clicks: 200,
+      impressions: 60000
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <ul>
+          <li>Select zero to N <span>Datasources</span></li>
+          <li>Select zero to N <span>Campaigns</span></li>
+        </ul>
+        <p><small>(Where zero means "All")</small></p>
+        <p>Hitting "Apply", filters the chart to show a timeseries for both Clicks</p>
       </header>
+
+      <main>
+        <aside>
+          <h1>Filter dimension values</h1>
+          
+          <div>
+            <span>Datasource</span>
+            <i>toggle</i>
+            <button
+              disabled
+              type="button"
+            >Reset</button>
+          </div>
+          <div>
+            <p>Doubleclick (dfa)</p>
+            <p>Meetrics</p>
+            <button type="button">Apply</button>
+          </div>
+
+          <div>
+            <span>Campaign</span>
+            <i>toggle</i>
+            <button
+              disabled
+              type="button"
+            >Reset</button>
+          </div>
+          <div>
+            <p>All</p>
+            <button type="button">Apply</button>
+          </div>
+        </aside>
+
+        <section>
+          <h1>Datasource "Doubleclick (dfa)" and "Meetrics"; All Campaigns</h1>
+          <div>Chart</div>
+        </section>
+      </main>
     </div>
   );
 }
