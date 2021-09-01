c9e033707615202a7752f708a35d12d740e18459
Giuseppe Leo <giuseppe.leo@retresco.de>
2021-09-01 16:41:06 +0200

    Correct date format

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,10 +1,19 @@
+import { format, parse } from 'date-fns';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 interface ChartProps {
   data: ChartData[],
 }
 
-export const Chart = ({data}: ChartProps) => {
+export const Chart = ({ data }: ChartProps) => {
+  const formatDate = (date: string) => {
+    if (!date) {
+      return date;
+    }
+    const parsedDate = parse(date, 'dd.MM.yyyy', new Date())
+    return format(parsedDate, 'dd.LLL')
+  }
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart
@@ -19,15 +28,30 @@ export const Chart = ({data}: ChartProps) => {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date"/>
+        <XAxis
+          dataKey="date"
+          label={{ angle: -90 }}
+          interval={7}
+          tick={({ x, y, stroke, payload }) => (
+            <g transform={`translate(${x},${y})`}>
+              <text
+                x={0}
+                y={0}
+                dy={16}
+                textAnchor="middle"
+                fill="#666"
+              >{formatDate(payload.value)}</text>
+            </g>
+          )}
+        />
         <YAxis
           yAxisId="left"
-          label={{ value: 'Clicks', angle: -90, position: 'insideLeft', offset: -5 }} 
+          label={{ value: 'Clicks', angle: -90, position: 'insideLeft', offset: -5 }}
         />
         <YAxis
           yAxisId="right"
           orientation="right"
-          label={{ value: 'Impressions', angle: -90, position: 'insideRight', offset: -25 }} 
+          label={{ value: 'Impressions', angle: -90, position: 'insideRight', offset: -25 }}
         />
         <Tooltip />
         <Legend />
